e58ab551c7d9c22ef60536b8afe0e6926f23ff9b
Benny <bennyhinnotieno@gmail.com>
2020-11-01 16:14:52 +0300

    modified navigation to colorpalette

--- screens/Home.js
+++ screens/Home.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { View, Text, TouchableOpacity } from 'react-native';
+import { View, FlatList, Text, TouchableOpacity } from 'react-native';
 // const COLORS = [...];
 
 const SOLARIZED = [
@@ -20,22 +20,64 @@ const SOLARIZED = [
   { colorName: 'Cyan', hexCode: '#2aa198' },
   { colorName: 'Green', hexCode: '#859900' },
 ];
+const RAINBOW = [
+  { colorName: 'Red', hexCode: '#FF0000' },
+  { colorName: 'Orange', hexCode: '#FF7F00' },
+  { colorName: 'Yellow', hexCode: '#FFFF00' },
+  { colorName: 'Green', hexCode: '#00FF00' },
+  { colorName: 'Violet', hexCode: '#8B00FF' },
+];
+
+const FRONTEND_MASTERS = [
+  { colorName: 'Red', hexCode: '#c02d28' },
+  { colorName: 'Black', hexCode: '#3e3e3e' },
+  { colorName: 'Grey', hexCode: '#8a8a8a' },
+  { colorName: 'White', hexCode: '#ffffff' },
+  { colorName: 'Orange', hexCode: '#e66225' },
+];
+
+const COLOR_PALETTES = [
+  { paletteName: 'Solarized', colors: SOLARIZED },
+  { paletteName: 'Frontend Masters', colors: FRONTEND_MASTERS },
+  { paletteName: 'Rainbow', colors: RAINBOW },
+];
 
 const Home = ({ navigation }) => {
   return (
-    <View>
-      <TouchableOpacity
-        onPress={() => {
-          navigation.navigate('ColorPalette', {
-            paletteName: 'Solarized',
-            colors: SOLARIZED,
-          });
-        }}
-      >
-        <Text>Solarized</Text>
-      </TouchableOpacity>
-    </View>
+    <FlatList
+      data={COLOR_PALETTES}
+      keyExtractor={(item) => item.paletteName}
+      renderItem={({ item }) => (
+        <TouchableOpacity
+          onPress={() => {
+            //   since paletteName and colors are the same u can just pass in the entire item
+            // // {
+            //   paletteName: 'Solarized',
+            //   colors: SOLARIZED,
+            // }
+            navigation.navigate('ColorPalette', item);
+          }}
+        >
+          <Text>{item.paletteName}</Text>
+        </TouchableOpacity>
+      )}
+    />
   );
 };
+
+{
+  /* <View>
+<TouchableOpacity
+  onPress={() => {
+    navigation.navigate('ColorPalette', {
+      paletteName: 'Solarized',
+      colors: SOLARIZED,
+    });
+  }}
+>
+  <Text>Solarized</Text>
+</TouchableOpacity>
+</View> */
+}
 
 export default Home;
